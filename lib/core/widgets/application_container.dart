953aa85f967b55c3a1758f708d574dfb9b7e26d3
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2023-12-30 01:02:50 +0800

    ♻️ refactor: ApplicationContainer to accept a single child widget

--- lib/core/widgets/application_container.dart
+++ lib/core/widgets/application_container.dart
@@ -1,15 +1,12 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_offline/flutter_offline.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:responsive_framework/responsive_framework.dart';
-
-import 'package:otoscopia/core/core.dart';
 
 import 'offline_bottom_bar.dart';
 
 class ApplicationContainer extends ConsumerWidget {
-  const ApplicationContainer({super.key, required this.children});
-  final List<ResponsiveRowColumnItem> children;
+  const ApplicationContainer({super.key, required this.child});
+  final Widget child;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -27,7 +24,7 @@ class ApplicationContainer extends ConsumerWidget {
             ],
           );
         },
-        child: ResponsiveContainer(children: children),
+        child: child,
       ),
     );
   }
