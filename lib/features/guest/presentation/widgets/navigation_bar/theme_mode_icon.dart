76ab2019e172638530103423b267ffec0da32368
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-01 12:18:31 +0800

    ✨ feat (widget): Add ThemeModeIcon widget for navigation bar

---
+++ lib/features/guest/presentation/widgets/navigation_bar/theme_mode_icon.dart
@@ -0,0 +1,26 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ionicons/ionicons.dart';
+
+import 'package:otoscopia/core/core.dart';
+
+class ThemeModeIcon extends ConsumerWidget {
+  const ThemeModeIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    ThemeMode themeMode = ref.watch(themeProvider);
+    IoniconsData iconData = themeMode == ThemeMode.system
+        ? Ionicons.invert_mode
+        : themeMode == ThemeMode.light
+            ? Ionicons.sunny
+            : Ionicons.moon;
+
+    return IconButton(
+      icon: Icon(iconData, size: 18),
+      onPressed: () => ref.read(themeProvider.notifier).toggleTheme(),
+    );
+  }
+}
