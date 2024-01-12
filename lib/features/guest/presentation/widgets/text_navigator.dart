870649fef846c26821dbb0d3078d9b6d31a6e2d5
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-13 01:22:44 +0800

    💄 style (widget): Add bold option to TextNavigator widget

--- lib/features/guest/presentation/widgets/text_navigator.dart
+++ lib/features/guest/presentation/widgets/text_navigator.dart
@@ -1,13 +1,16 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:styled_widget/styled_widget.dart';
 
 class TextNavigator extends StatelessWidget {
   const TextNavigator(
     this.text, {
     super.key,
     required this.child,
+    this.bold = true,
   });
   final String text;
   final String child;
+  final bool bold;
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +21,7 @@ class TextNavigator extends StatelessWidget {
           return FluentTheme.of(context).typography.body!.color;
         }),
       ),
-      child: Text(text),
+      child: Text(text).fontWeight(bold ? null : FontWeight.normal),
       onPressed: () => Navigator.pushNamed(context, child),
     );
   }
