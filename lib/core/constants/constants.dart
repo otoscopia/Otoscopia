0c3461e6bb33477b414166bf0a113d3af7b053b0
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-13 13:48:57 +0800

    📝 build (utils): Update imports and exports in constants.dart

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,10 +1,12 @@
-import 'guest_constants.dart';
+import 'named_routes_constants.dart';
 
+export 'button_constants.dart';
 export 'guest_constants.dart';
+export 'named_routes_constants.dart';
 
 const String kAppName = 'Otoscopia';
 const String kMultiPlatform = 'A Multi-Platform Application';
 const String kDiscoverYourEarHealth = 'Discover your ear health with a simple test, and get a diagnosis with the help of a doctor.';
 const String kOffline = 'Offline';
-const String kInitialRoute = GuestNamedRoutes.home;
+const String kInitialRoute = NamedGuest.home;
 const String kLogo = 'assets/otoscopia.svg';
