72971a990f9a8fc77a0b1946defbc0ad46ae9d7e
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-15 02:40:10 +0800

    ♻️ refactor (entity): TableEntity now uses Assignment and Screening entities

--- lib/core/entities/table_entity.dart
+++ lib/core/entities/table_entity.dart
@@ -5,33 +5,41 @@ import 'package:otoscopia/core/core.dart';
 
 class TableEntity {
   final PatientEntity patient;
-  final String status;
+  final AssignmentEntity assignment;
+  final ScreeningEntity screening;
   TableEntity({
     required this.patient,
-    required this.status,
+    required this.assignment,
+    required this.screening,
   });
 
   TableEntity copyWith({
     PatientEntity? patient,
-    String? status,
+    AssignmentEntity? assignment,
+    ScreeningEntity? screening,
   }) {
     return TableEntity(
       patient: patient ?? this.patient,
-      status: status ?? this.status,
+      assignment: assignment ?? this.assignment,
+      screening: screening ?? this.screening,
     );
   }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'patient': patient.toMap(),
-      'status': status,
+      'assignment': assignment.toMap(),
+      'screening': screening.toMap(),
     };
   }
 
   factory TableEntity.fromMap(Map<String, dynamic> map) {
     return TableEntity(
       patient: PatientEntity.fromMap(map['patient'] as Map<String, dynamic>),
-      status: map['status'] as String,
+      assignment:
+          AssignmentEntity.fromMap(map['assignment'] as Map<String, dynamic>),
+      screening:
+          ScreeningEntity.fromMap(map['screening'] as Map<String, dynamic>),
     );
   }
 
@@ -41,15 +49,19 @@ class TableEntity {
       TableEntity.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() => 'TableEntity(patient: $patient, status: $status)';
+  String toString() =>
+      'TableEntity(patient: $patient, assignment: $assignment, screening: $screening)';
 
   @override
   bool operator ==(covariant TableEntity other) {
     if (identical(this, other)) return true;
 
-    return other.patient == patient && other.status == status;
+    return other.patient == patient &&
+        other.assignment == assignment &&
+        other.screening == screening;
   }
 
   @override
-  int get hashCode => patient.hashCode ^ status.hashCode;
+  int get hashCode =>
+      patient.hashCode ^ assignment.hashCode ^ screening.hashCode;
 }
