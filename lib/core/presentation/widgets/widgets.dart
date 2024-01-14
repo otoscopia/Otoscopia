b44e815d2bee8c00176b4c5a067ab9d1c6bd4ef8
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-14 16:47:03 +0800

    🚚 chore: Move providers and widgets to presentation folder

--- lib/core/presentation/widgets/widgets.dart
+++ lib/core/presentation/widgets/widgets.dart
@@ -2,6 +2,7 @@ export 'application_container.dart';
 export 'logo.dart';
 export 'pop_up_info_bar.dart';
 export 'responsive_builder.dart';
+export 'search_box.dart';
 
 export 'text/display_text.dart';
 export 'text/title_text.dart';
