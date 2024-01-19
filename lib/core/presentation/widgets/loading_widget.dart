514aedb955ebef895fd0b9c639cac98908ac2085
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-19 17:18:55 +0800

    ✨ feat (widget): Add loading screen

---
+++ lib/core/presentation/widgets/loading_widget.dart
@@ -0,0 +1,22 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
+
+import 'package:otoscopia/core/core.dart';
+
+class LoadingWidget extends ConsumerWidget {
+  const LoadingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Logo(),
+        Gap(8),
+        ProgressBar(),
+      ],
+    );
+  }
+}
