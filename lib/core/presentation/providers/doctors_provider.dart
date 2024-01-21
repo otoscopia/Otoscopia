a3a27b9c3da567d7cb64f74e5560b7d420253398
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-22 03:38:36 +0800

    🚩 chore (provider): Update DoctorsNotifier to use UsersEntity instead of UserEntity

--- lib/core/presentation/providers/doctors_provider.dart
+++ lib/core/presentation/providers/doctors_provider.dart
@@ -2,13 +2,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:otoscopia/core/core.dart';
 
-class DoctorsNotifier extends StateNotifier<List<UserEntity>> {
+class DoctorsNotifier extends StateNotifier<List<UsersEntity>> {
   DoctorsNotifier() : super([]);
 
-  void setUsers(List<UserEntity> users) => state = users;
+  void setDoctors(List<UsersEntity> users) => state = users;
 }
 
 final doctorsProvider =
-    StateNotifierProvider<DoctorsNotifier, List<UserEntity>>((ref) {
+    StateNotifierProvider<DoctorsNotifier, List<UsersEntity>>((ref) {
   return DoctorsNotifier();
 });
