14f07948b489b77168be23ef1741cee46948ec2d
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-22 04:35:59 +0800

    ✨ feat (provider): Create nures provider

---
+++ lib/core/presentation/providers/nurses_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:otoscopia/core/core.dart';
+
+class NursesNotifier extends StateNotifier<List<UsersEntity>> {
+  NursesNotifier() : super([]);
+
+  void setNurses(List<UsersEntity> nurses) => state = nurses;
+}
+
+final nursesProvider = StateNotifierProvider<NursesNotifier, List<UsersEntity>>(
+  (ref) => NursesNotifier(),
+);
