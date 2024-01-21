c72571d8d28f2c0c2f4deec395a0f48ac1aae6e6
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-22 02:53:03 +0800

    ✨ feat (usecases): Add FetchDataUseCase class that uses FetchDataRepository interface

---
+++ lib/core/domain/usecases/fetch_data_usecase.dart
@@ -0,0 +1,31 @@
+import 'package:otoscopia/core/core.dart';
+
+class FetchDataUseCase {
+  final FetchDataRepository _fetchDataRepository;
+
+  FetchDataUseCase(this._fetchDataRepository);
+
+  Future<List<AssignmentEntity>> getAssignments() async {
+    return await _fetchDataRepository.getAssignments();
+  }
+
+  Future<List<PatientEntity>> getPatients() async {
+    return await _fetchDataRepository.getPatients();
+  }
+
+  Future<List<UsersEntity>> getDoctors() async {
+    return await _fetchDataRepository.getDoctors();
+  }
+
+  Future<List<UsersEntity>> getNurses() async {
+    return await _fetchDataRepository.getNurses();
+  }
+
+  Future<List<SchoolEntity>> getSchools() async {
+    return await _fetchDataRepository.getSchools();
+  }
+
+  Future<List<ScreeningEntity>> getScreenings() async {
+    return await _fetchDataRepository.getScreenings();
+  }
+}
