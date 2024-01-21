143de8cae9ee752e859a384cb8622fd97f5d65f5
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-22 03:58:42 +0800

    🚩 feat (provider): Add findByName and findById methods

--- lib/core/presentation/providers/schools_provider.dart
+++ lib/core/presentation/providers/schools_provider.dart
@@ -6,6 +6,16 @@ class SchoolsNotifier extends StateNotifier<List<SchoolEntity>> {
   SchoolsNotifier() : super([]);
 
   void setSchools(List<SchoolEntity> schools) => state = schools;
+
+  SchoolEntity findByName(String name) {
+    final SchoolEntity school = state.firstWhere((school) => school.abbr == name);
+    return school;
+  }
+
+  SchoolEntity findById(String id) {
+    final SchoolEntity school = state.firstWhere((school) => school.id == id);
+    return school;
+  }
 }
 
 final schoolsProvider =
