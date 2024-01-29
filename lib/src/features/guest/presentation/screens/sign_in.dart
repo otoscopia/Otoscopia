811a567aa87cda72710c7481d4241690c73abe19
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-29 10:13:24 +0800

    🚩feat (page): Add onEditingComplete callback to PasswordFormBox, and modify onClick function

--- lib/src/features/guest/presentation/screens/sign_in.dart
+++ lib/src/features/guest/presentation/screens/sign_in.dart
@@ -44,6 +44,7 @@ class _SignInState extends ConsumerState<SignIn> {
                 PasswordFormBox(
                   controller: passwordController,
                   placeholder: kPassword,
+                  onEditingComplete: onClick,
                 ),
                 const Gap(16),
                 const TextNavigator(
@@ -82,11 +83,13 @@ class _SignInState extends ConsumerState<SignIn> {
     setState(() => isLoading = true);
     if (formKey.currentState!.validate()) {
       try {
-        bool response = await ref
+        final response = await ref
             .read(authenticationProvider.notifier)
             .login(email, password);
 
-        if (response) {
+        if (response.id.isNotEmpty) {
+          await ref.read(fetchDataProvider.notifier).fetch(response);
+
           WidgetsBinding.instance.addPostFrameCallback((_) {
             Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
           });
@@ -99,8 +102,9 @@ class _SignInState extends ConsumerState<SignIn> {
             context,
           ),
         );
+      } finally {
+        setState(() => isLoading = false);
       }
     }
-    setState(() => isLoading = false);
   }
 }
