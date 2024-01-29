79122d584a1087baa819ad292f819622c2f5c813
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-30 01:38:42 +0800

    ♻️ refactor (provider): Add setImage method

--- lib/src/features/nurse/presentation/providers/screening_information_provider.dart
+++ lib/src/features/nurse/presentation/providers/screening_information_provider.dart
@@ -6,11 +6,15 @@ import 'package:otoscopia/src/features/nurse/nurse.dart';
 class ScreeningInformationNotifier extends StateNotifier<ScreeningEntity> {
   ScreeningInformationNotifier() : super(ScreeningEntity.initial());
 
-  void setScreening(WidgetRef ref, MedicalFormEntity medical, List<String> images) {
+  void setScreening(WidgetRef ref, MedicalFormEntity medical) {
     final user = ref.read(userProvider);
     final patient = ref.read(patientProvider);
     final assignment = ref.read(assignmentsProvider.notifier).findByNurseAndSchool(user.id, patient.school);
-    state = ScreeningEntity.fromMedical(medical, patient.id, assignment.id, images);
+    state = ScreeningEntity.fromMedical(medical, patient.id, assignment.id, state.images);
+  }
+
+  void setImage(String image) {
+    state = state.copyWith(images: [...state.images, image]);
   }
 
   void resetInformation() => state = ScreeningEntity.initial();
