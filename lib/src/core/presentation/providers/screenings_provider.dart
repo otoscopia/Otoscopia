1a8a65796569faa434ea4d87ca66d3bed8208e02
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-01-30 00:46:35 +0800

    ✨ feat (provider): Add findByPatientId, removeScreening and addScreening method

--- lib/src/core/presentation/providers/screenings_provider.dart
+++ lib/src/core/presentation/providers/screenings_provider.dart
@@ -6,6 +6,21 @@ class ScreeningsNotifier extends StateNotifier<List<ScreeningEntity>> {
   ScreeningsNotifier() : super([]);
 
   void setScreenings(List<ScreeningEntity> screenings) => state = screenings;
+
+  void removeScreening(int index) => state.removeAt(index);
+
+  List<ScreeningEntity> findByPatientId(String patientId) {
+    return state.where((screening) => screening.patient == patientId).toList();
+  }
+
+  void addScreening(ScreeningEntity screening) {
+    final index = state.indexWhere((element) => element.id == screening.id);
+    if (index >= 0) {
+      removeScreening(index);
+    }
+
+    state = [...state, screening];
+  }
 }
 
 final screeningsProvider =
