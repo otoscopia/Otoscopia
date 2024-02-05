09c1a56c924d8d100251f6e54aa8cca9c2cdb48f
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-02-06 02:09:54 +0800

    🚀 Deploy: Optimize fetching data from database & add camera pause feature when capture is clicked.

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -32,9 +32,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "3cfb85d35b0a47877c5c3caddb08afb9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "03db662d7bca2b06ca6fcf41cd8004d5",
-"/": "03db662d7bca2b06ca6fcf41cd8004d5",
-"main.dart.js": "cbd863af38f6ed851c1790eeefd01ca0",
+"index.html": "565987d6b5fd9f52cfd33dd210539f5a",
+"/": "565987d6b5fd9f52cfd33dd210539f5a",
+"main.dart.js": "dce631d933cc886fbc67dc06fea59d6b",
 "manifest.json": "66402b331f7764b92f034323b5ccfdaa",
 "style.css": "7a2f3526589a61283e7d40fa33c49617",
 "version.json": "3530722605e7fa4cc387cd7d906cfb67"};
