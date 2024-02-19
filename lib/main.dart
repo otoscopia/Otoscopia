942a2b3ca054dc7682fae20bcb0d92e783a09b20
Laurence Troy Sambaa Valdez <laurencetroyv@gmail.com>
2024-02-20 06:03:22 +0800

    ✨ feat (main): Add Sentry integration and initialize it with release information

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
+import 'package:otoscopia/application.dart';
 import 'package:otoscopia/src/app.dart';
 import 'package:otoscopia/src/config/config.dart';
 import 'package:otoscopia/src/core/core.dart';
 
 Future<void> main() async {
+  const sentryRelease = '$kPackageName@${ApplicationConfig.currentVersion}';
+
   WidgetsFlutterBinding.ensureInitialized();
 
   DeviceType deviceType = getDeviceType();
 
-  DependencyInjection().init(deviceType);
+  await DependencyInjection().init(deviceType);
 
-  runApp(
-    ProviderScope(
-      child: MyApp(deviceType),
-    ),
-  );
+  await SentryFlutter.init((options) {
+    options.dsn = Env.sentryApi;
+    options.release = sentryRelease;
+  }, appRunner: () {
+    return runApp(
+      DefaultAssetBundle(
+        bundle: SentryAssetBundle(),
+        child: ProviderScope(
+          child: MyApp(deviceType),
+        ),
+      ),
+    );
+  });
 }
