a0fcd6ef7eb87d9bf73c92030ec829cdca161641
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-04-04 14:42:50 +0800

    ✨ feat (widget): Add mobile support

--- lib/src/core/presentation/screens/dashboard.dart
+++ lib/src/core/presentation/screens/dashboard.dart
@@ -8,12 +8,19 @@ class Dashboard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final isWeb = getDeviceType() == DeviceType.web;
+    final isMobile = getDeviceType() == DeviceType.mobile;
+
+    final mobile = isMobile == true ? isMobile : isWeb && MediaQuery.of(context).size.width < 400;
+    
     return TabView(
       closeDelayDuration: Duration.zero,
       tabs: ref.watch(dashboardTabProvider),
       shortcutsEnabled: true,
       currentIndex: ref.watch(dashboardIndexProvider),
-      closeButtonVisibility: CloseButtonVisibilityMode.onHover,
+      closeButtonVisibility: mobile
+          ? CloseButtonVisibilityMode.always
+          : CloseButtonVisibilityMode.onHover,
       onChanged: (index) {
         ref.read(dashboardIndexProvider.notifier).setIndex(index);
       },
