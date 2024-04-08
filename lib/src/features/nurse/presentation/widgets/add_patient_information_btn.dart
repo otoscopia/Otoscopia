aca3b0164ca37e227f88487fa5afb3be5ed7fcfe
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-04-08 11:37:01 +0800

    ♻️ refactor: Update AddPatientInformationBtn widget to use FilledButton instead of Button

--- lib/src/features/nurse/presentation/widgets/add_patient_information_btn.dart
+++ lib/src/features/nurse/presentation/widgets/add_patient_information_btn.dart
@@ -1,7 +1,6 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:otoscopia/src/config/config.dart';
 import 'package:otoscopia/src/core/core.dart';
 import 'package:otoscopia/src/features/nurse/nurse.dart';
 
@@ -14,7 +13,7 @@ class AddPatientInformationBtn extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     bool hasValue = ref.read(patientProvider).id.isNotEmpty;
 
-    return Button(
+    return FilledButton(
       onPressed: () {
         bool isValid = _form.isValid;
 
@@ -31,20 +30,6 @@ class AddPatientInformationBtn extends ConsumerWidget {
         ref.read(patientProvider.notifier).setPatient(ref, _form, hasValue);
         ref.read(addPatientTabProvider.notifier).addLeftCamera();
       },
-      style: ButtonStyle(
-        backgroundColor: ButtonState.resolveWith((states) {
-          if (states.contains(ButtonStates.hovering)) {
-            return AppColors.accentColor.darker;
-          }
-          return AppColors.accentColor.dark;
-        }),
-        foregroundColor: ButtonState.resolveWith((states) {
-          if (states.contains(ButtonStates.hovering)) {
-            return Colors.black;
-          }
-          return null;
-        }),
-      ),
       child: Text(hasValue ? kUpdateBtn : kContinueBtn),
     );
   }
