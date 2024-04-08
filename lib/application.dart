fa31879e599da428900ce94216b5854a719553d7
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-04-08 11:31:34 +0800

    ♻️ refactor (app): Modify fetchReleases method in ApplicationConfig to use ReleaseEntity instead of Release

--- lib/application.dart
+++ lib/application.dart
@@ -9,11 +9,11 @@ class ApplicationConfig {
   static const kRepository =
       "https://api.github.com/repos/otoscopia/otoscopia/releases";
 
-  Future<List<Release>> fetchReleases() async {
+  Future<List<ReleaseEntity>> fetchReleases() async {
     final response = await http.get(Uri.parse(kRepository));
     if (response.statusCode == 200) {
       final List<dynamic> responseData = json.decode(response.body);
-      return responseData.map((e) => Release.fromJson(e)).toList();
+      return responseData.map((e) => ReleaseEntity.fromJson(e)).toList();
     } else {
       throw Exception('Failed to load releases');
     }
