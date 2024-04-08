358f2d1e860ea492658851bba7bc84e0e5948638
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-04-08 11:32:10 +0800

    ♻️ refactor: Modify theme configuration and merge dark_theme and light_theme

--- lib/src/config/themes/colors.dart
+++ lib/src/config/themes/colors.dart
@@ -6,5 +6,19 @@ class AppColors {
   static const Color secondary = Color(0xFF005858);
   static const Color transparent = Colors.transparent;
 
-  static final accentColor = SystemTheme.accentColor.accent.toAccentColor();
+  final _accentColor = SystemTheme.accentColor.accent.toAccentColor();
+
+  AccentColor get systemAccent => _accentColor;
+
+  List<Map<String, Object>> get accentColors => [
+        {"color": systemAccent, "name": "System"},
+        {"color": Colors.yellow, "name": "Light Yellow"},
+        {"color": Colors.orange, "name": "Orange"},
+        {"color": Colors.red, "name": "Red"},
+        {"color": Colors.magenta, "name": "Pink"},
+        {"color": Colors.purple, "name": "Lavander"},
+        {"color": Colors.blue, "name": "Blue"},
+        {"color": Colors.teal, "name": "Teal"},
+        {"color": Colors.green, "name": "Green"},
+      ];
 }
--- lib/src/config/themes/themes.dart
+++ lib/src/config/themes/themes.dart
@@ -1,3 +1,28 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+import 'package:otoscopia/src/features/settings/settings.dart';
+
 export 'colors.dart';
-export 'dark_theme.dart';
-export 'light_theme.dart';
+
+class ThemeEntity {
+  final SettingsEntity _settings;
+
+  ThemeEntity(this._settings);
+
+  FluentThemeData get lightMode {
+    return FluentThemeData(
+      accentColor: _settings.accentColor,
+      visualDensity: VisualDensity.comfortable,
+    );
+  }
+
+  FluentThemeData get darkMode {
+    return FluentThemeData(
+      brightness: Brightness.dark,
+      accentColor: _settings.accentColor,
+      visualDensity: VisualDensity.comfortable,
+    );
+  }
+
+  ThemeMode get themeMode => _settings.themeMode;
+}
