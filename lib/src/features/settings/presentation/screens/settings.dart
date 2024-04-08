b3d600a432cf1a3da597ddb7cb27e5a3bb8af209
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-04-08 11:40:07 +0800

    ♻️ refactor (page): Modify settings screen UI

--- lib/src/features/settings/presentation/screens/settings.dart
+++ lib/src/features/settings/presentation/screens/settings.dart
@@ -11,103 +11,27 @@ class Settings extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return DoubleCard(
-      scroll: ref.watch(fontSizeProvider) >= 1.1 ? true : false,
+    return const DoubleCard(
+      scroll: true,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const CustomText(kSettings, style: 1),
-          const Gap(8),
-          ListTile(
-            title: const CustomText(kDarkMode),
-            subtitle: const CustomText(kToggleDarkMode, style: 3),
-            trailing: ToggleSwitch(
-              checked: ref.watch(themeProvider) == ThemeMode.dark,
-              onChanged: (value) =>
-                  ref.read(themeProvider.notifier).toggleTheme(),
-            ),
+          CustomText('Settings', style: 1),
+          Gap(12),
+          CustomText('Display', style: 5),
+          Gap(8),
+          SettingsExpander(
+            icon: FluentIcons.color_solid,
+            title: "Colors",
+            subtitle: "Modify Theme Mode and Accent Colors",
+            content: ColorsSettings(),
           ),
-          ListTile(
-            title: const CustomText(kFontSize),
-            subtitle: const CustomText(kChangeFontSize, style: 3),
-            trailing: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const CustomText(kSM),
-                const Gap(12),
-                Slider(
-                  value: ref.watch(fontSizeProvider),
-                  onChanged: (value) =>
-                      ref.read(fontSizeProvider.notifier).changeFontSize(value),
-                  min: 0.5,
-                  max: 2.0,
-                  divisions: 10,
-                ),
-                const Gap(12),
-                Text(
-                  kLG,
-                  style: FluentTheme.of(context).typography.body,
-                  textScaler: const TextScaler.linear(2),
-                ),
-              ],
-            ),
-          ),
-          const ListTile(
-            title: CustomText(kApplicationStorage),
-            subtitle: CustomText(kChangeApplicationStorage, style: 3),
-            // trailing: IconButton(
-            //   icon: const Icon(Icons.folder),
-            //   onPressed: () => ref
-            //       .read(applicationStorageProvider.notifier)
-            //       .changeApplicationStorage(),
-            // ),
-          ),
-          const ListTile(
-            title: CustomText(kCache),
-            subtitle: CustomText(kClearCache, style: 3),
-            // trailing: IconButton(
-            //   icon: const Icon(Icons.delete),
-            //   onPressed: () => ref.read(cacheProvider.notifier).clearCache(),
-            // ),
-          ),
-          const ListTile(
-            title: CustomText(kEnableEmailNotifications),
-            subtitle: CustomText(kToggleEmailNotifications, style: 3),
-            // trailing: ToggleSwitch(
-            //   checked: ref.watch(emailNotificationsProvider),
-            //   onChanged: (value) =>
-            //       ref.read(emailNotificationsProvider.notifier).toggleEmail(),
-            // ),
-          ),
-          const ListTile(
-            title: CustomText(kEnablePinLock),
-            subtitle: CustomText(kTogglePinLock, style: 3),
-            // trailing: ToggleSwitch(
-            //   checked: ref.watch(pinLockProvider),
-            //   onChanged: (value) =>
-            //       ref.read(pinLockProvider.notifier).togglePinLock(),
-            // ),
-          ),
-          const ListTile(
-            title: CustomText(kMasterPassword),
-            subtitle: CustomText(kChangeMasterPassword, style: 3),
-            // trailing: IconButton(
-            //   icon: const Icon(Icons.lock),
-            //   onPressed: () => ref
-            //       .read(masterPasswordProvider.notifier)
-            //       .changeMasterPassword(),
-            // ),
-          ),
-          const ListTile(
-            title: CustomText(kAccountPassword),
-            subtitle: CustomText(kChangeAccountPassword, style: 3),
-            // trailing: IconButton(
-            //   icon: const Icon(Icons.lock),
-            //   onPressed: () => ref
-            //       .read(accountPasswordProvider.notifier)
-            //       .changeAccountPassword(),
-            // ),
+          Gap(4),
+          SettingsExpander(
+            icon: FluentIcons.font_color,
+            title: "Text Size",
+            subtitle: "Modify Text Size and Font Family",
+            content: TextSettings(),
           ),
         ],
       ),
