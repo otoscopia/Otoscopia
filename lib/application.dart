fb1bb0f3f8a0c69b4d9abd8aec94f02cf3c581b7
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-04-08 10:56:06 +0800

    ✨ feat: Add fetchReleases method to ApplicationConfig class

--- lib/application.dart
+++ lib/application.dart
@@ -1,3 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:otoscopia/src/core/core.dart';
+
 class ApplicationConfig {
   static const String currentVersion = "0.3.0";
+  static const kRepository =
+      "https://api.github.com/repos/otoscopia/otoscopia/releases";
+
+  Future<List<Release>> fetchReleases() async {
+    final response = await http.get(Uri.parse(kRepository));
+    if (response.statusCode == 200) {
+      final List<dynamic> responseData = json.decode(response.body);
+      return responseData.map((e) => Release.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to load releases');
+    }
+  }
 }
+
